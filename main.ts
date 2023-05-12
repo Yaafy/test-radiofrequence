05aaf8cc01765a6c38878ab1fd6d9b09a2e34240
Yaafy <132677932+Yaafy@users.noreply.github.com>
2023-05-12 17:36:49 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+radio.setGroup(68)
 basic.forever(function () {
-	
+    radio.sendNumber(7)
+    radio.sendString("f u hehe")
 })
